771db234446316cbe322752571e9a82c476eb639
iTanvirSingh <71079808+tanviirsinghh@users.noreply.github.com>
2024-07-11 15:27:47 +0530

    Updated the database url

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ const Config = {
     "HOST": "127.0.0.1",
     "PORT": 5050,
     "DATABASE": {
-        URL: "mongodb://127.0.0.1:27017/Playpal"
+        URL: "mongodb+srv://yanon6107:x1dKvHveqAjg8An4@contactform.iztmlxc.mongodb.net/"
     },
     "JWTSECRETKEY": "VISHAL"
 }
