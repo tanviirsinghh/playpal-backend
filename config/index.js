2e4e0d36e2a1c6e396158efc1e098f7418b73966
iTanvirSingh <71079808+tanviirsinghh@users.noreply.github.com>
2024-07-11 20:07:36 +0530

    Update db string again

--- config/index.js
+++ config/index.js
@@ -2,7 +2,8 @@ const Config = {
     "HOST": "127.0.0.1",
     "PORT": 5050,
     "DATABASE": {
-        URL: "mongodb+srv://yanon6107:x1dKvHveqAjg8An4@contactform.iztmlxc.mongodb.net/"
+        URL: "mongodb+srv://yanon6107:x1dKvHveqAjg8An4@contactform.iztmlxc.mongodb.net/myDatabase?retryWrites=true&w=majority"
+ 
     },
     "JWTSECRETKEY": "VISHAL"
 }
