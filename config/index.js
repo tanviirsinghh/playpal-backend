032a63ed299b722dbc3edb06ef0feb83a0aa8613
iTanvirSingh <71079808+tanviirsinghh@users.noreply.github.com>
2024-07-11 22:13:39 +0530

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const Config = {
     "HOST": "127.0.0.1",
-    "PORT": 5050,
+    "PORT": 3000,
     "DATABASE": {
         URL: "mongodb+srv://yanon6107:x1dKvHveqAjg8An4@contactform.iztmlxc.mongodb.net/"
  
